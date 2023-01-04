a2000394c71c259836e4a2c55c3637fbb64d3ee3
Sandoct <gpouzet42@gmail.com>
2023-01-04 18:09:17 +0100

    first commit with vscodium

--- push_swap.c
+++ push_swap.c
@@ -9,9 +9,27 @@
 /*   Updated: 2022/12/19 17:01:55 by gpouzet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft/libft.h"
 
 void	push_swap(int argc,int **argv)
 {
-	if (parser(argv[1]))
+	int	**separator;
+	int	**stacka;
+	int	**stackb;
+	int	i;
+
+	// prelude au programme
+	
+	if (!parser(argv[1]))
 		ft_printf("Error\n");
+	separator = ft_split(argv[1], " ");
+	stacka = ft_calloc(sizeof(int), ft_strlen(separator));
+	stackb = ft_calloc(sizeof(int), ft_strlen(separator));
+	i = 0;
+	while (separator[i])
+		stacka[i] = ft_atoi(separator[ft_strlen(separator) - i++]);			// affectation en reverse 
+	free(separator);														//free l'entiereté si necesaire
+	
+	//trie de la stack
+
 }
---
+++ push_swap_utils.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap_utils.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpouzet <gpouzet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/04 16:48:42 by gpouzet           #+#    #+#             */
+/*   Updated: 2023/01/04 16:48:42 by gpouzet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft/libft/libft.h"
+#include "libft/ft_printf/ft_printf.h"
+
+void    swap(int *stacka, int stackb, char c)
+{
+    int     tmp;
+    size_t  size;
+    
+    size = ft
+    if (c == s)
+        ft_printf("ss");
+
+    tmp = stacka[0];
+    stacka[0] = stacka[1];
+    stacka[1] = tmp;
+}
